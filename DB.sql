c75ac8329958a5e3b5a4e2fa53332b6d7ddcf990
Igor Kulgachov <duckpool@yandex.ru>
2022-08-05 10:27:20 +0300

    for confluence

--- DB.sql
+++ DB.sql
@@ -1,23 +1,17 @@
-create schema schema_name;
+CREATE SCHEMA schema_name;
 
-create table schema_name.table_name
+CREATE TABLE schema_name.table_name
 (
-    id             serial
-        constraint table_name_pk
-            primary key,
-    table_row_int  int  not null,
-    table_row_text text not null
+    id 			   SERIAL CONSTRAINT table_name_pk PRIMARY KEY,
+    table_row_int  INT  NOT NULL,
+    table_row_text TEXT NOT NULL
 );
 
-INSERT INTO schema_name.table_name (id, table_row_int, table_row_text)
-VALUES (1, 12, 'first');
-INSERT INTO schema_name.table_name (id, table_row_int, table_row_text)
-VALUES (2, 234, 'second');
-INSERT INTO schema_name.table_name (id, table_row_int, table_row_text)
-VALUES (3, 54, 'third');
-INSERT INTO schema_name.table_name (id, table_row_int, table_row_text)
-VALUES (4, 123, 'fourth');
-INSERT INTO schema_name.table_name (id, table_row_int, table_row_text)
-VALUES (5, 534, 'fifth');
-INSERT INTO schema_name.table_name (id, table_row_int, table_row_text)
-VALUES (6, 231, 'sixth');
+ALTER TABLE schema_name.table_name REPLICA IDENTITY FULL;
+
+INSERT INTO schema_name.table_name (id, table_row_int, table_row_text) VALUES (1, 12, 'first');
+INSERT INTO schema_name.table_name (id, table_row_int, table_row_text) VALUES (2, 234, 'second');
+INSERT INTO schema_name.table_name (id, table_row_int, table_row_text) VALUES (3, 54, 'third');
+INSERT INTO schema_name.table_name (id, table_row_int, table_row_text) VALUES (4, 123, 'fourth');
+INSERT INTO schema_name.table_name (id, table_row_int, table_row_text) VALUES (5, 534, 'fifth');
+INSERT INTO schema_name.table_name (id, table_row_int, table_row_text) VALUES (6, 231, 'sixth');
